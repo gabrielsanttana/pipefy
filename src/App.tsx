11e817af30079602197306bf8c59506c09b8ac3f
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-09-14 18:31:33 -0300

    Install styled-components and add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import GlobalStyles from './GlobalStyles';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Pipefy</h1>
-    </div>
+    <>
+      <GlobalStyles />
+    </>
   );
 };
 
